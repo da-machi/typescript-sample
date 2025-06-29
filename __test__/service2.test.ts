60bdb7915bf2c4430a193cdee8cb8f71e8e048e9
da-machin <39872641+da-machi@users.noreply.github.com>
2025-06-29 22:51:45 +0900

    Update service2.test.ts

--- __test__/service2.test.ts
+++ __test__/service2.test.ts
@@ -1,1 +1,23 @@
+import { addNumbers } from '../src/service';
 
+describe('addNumbers', () => {
+  test('正の整数の加算', () => {
+    expect(addNumbers(2, 3)).toBe(5);
+  });
+
+  test('負の数の加算', () => {
+    expect(addNumbers(-4, -6)).toBe(-10);
+  });
+
+  test('0の加算', () => {
+    expect(addNumbers(0, 0)).toBe(0);
+  });
+
+  test('小数の加算', () => {
+    expect(addNumbers(1.5, 2.3)).toBeCloseTo(3.8);
+  });
+
+    test('aaa意図して失敗するケース 0の加算', () => {
+    expect(addNumbers(0, 0)).toBe(10);
+  });
+});
